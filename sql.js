d7d816c6e391f16b9cbf2215052554dfcc2d6cfe
Brant Campodonico <brantcampodonico@Brants-MacBook-Pro.local>
2018-04-14 13:51:39 -0500

    changed values query

--- sql.js
+++ sql.js
@@ -35,7 +35,7 @@ function readTable() {
 
 function createItem() {
     console.log('Inserting new item up for bidding!');
-    connection.query(`INSERT INTO great_bay(item,highest_bid,starting_bid) VALUES(?,?,?)`, {
+    connection.query(`INSERT INTO great_bay(item,highest_bid,starting_bid) VALUES(?)`, {
         item: process.argv[2],
         highest_bid: process.argv[3],
         starting_bid: process.argv[4],
